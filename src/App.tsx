d5d4474dd62f6951ec0c2bb516dfcca2b967865c
Abdullah Al Masoud <almasoud49@yahoo.com>
2024-08-26 20:07:26 +0600

    [DONE] Basic Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+<h1 className='text-center text-slate-100 font-bold mx-auto  border-y-8 p-5'>Welcome to my Bike Rental Reservation Service</h1>
+
+    </>
+  )
+}
+
+export default App
